c12f14a20d7a36aaf153115026948111507eaa90
shubhamkumar1203 <158853227+shubhamkumar1203@users.noreply.github.com>
2024-09-22 17:23:20 +0000

    comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,9 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log("2" === 2); // === It not only checks for values but it checks for its datatypes also
