5ae035ba7de5d197c47ca66d4747ebfc5899bd98
shubhamkumar1203 <158853227+shubhamkumar1203@users.noreply.github.com>
2024-09-22 16:34:30 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = 33
+let age = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// console.log(typeof age);
+
+let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN(Not a number)
+// true => 1; false = 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Shubham" => true
+
+let number = 33
+
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
